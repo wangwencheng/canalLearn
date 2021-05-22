2045136b5f8fde84fea086acc9918171ab41fac0
wwc <960075207@qq.com>
2021-05-23 00:38:24 +0800

    binlog 实时同步数据测试

---
+++ learn/src/test/java/com/wwc/canal/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwc.canal.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
